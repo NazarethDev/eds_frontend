70ebe4a1384e96bdbd93b53e71a9f31cf8e54b1c
NazarethDev <lorran.nazareth@gmail.com>
2024-11-28 18:46:31 -0300

    correção do link do Waze para dispositivos móveis - celulares

--- waze.js
+++ waze.js
@@ -1,12 +1,12 @@
 function openWaze() {
-    const destinationAddress = "https://www.waze.com/pt-BR/live-map/directions/br/sp/r.-ana-maria-sirani,-546?navigate=yes&to=place.ChIJUaGehy5kzpQRtHIyv_glIIg";
-    const destinationCoordinates = "waze://?ll=-23.548893465107557,-46.432153821794834&navigate=yes";
+    const destinationOnPc = "https://www.waze.com/pt-BR/live-map/directions/br/sp/r.-ana-maria-sirani,-546?navigate=yes&to=place.ChIJUaGehy5kzpQRtHIyv_glIIg";
+    const destinationOnMobile = "https://waze.com/ul/h6gyfp3j4k";
     
     const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
     if (isMobile) {
-        window.location.href = `waze://?q=${encodeURIComponent(destinationAddress)}&navigate=yes`;
+        window.location.href = `waze://?q=${encodeURIComponent(destinationOnMobile)}&navigate=yes`;
     } else {
-        window.open(`https://www.waze.com/ul?ll=${destinationAddress}&navigate=yes`, "_blank");
+        window.open(`https://www.waze.com/ul?ll=${destinationOnPc}`, "_blank");
     }
 }
