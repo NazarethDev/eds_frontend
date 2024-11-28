7cdede25f45cdbd01fa6cacb68f149f636a35f80
NazarethDev <lorran.nazareth@gmail.com>
2024-11-28 19:41:03 -0300

    sétima correção no link do Waze para mobile

--- waze.js
+++ waze.js
@@ -1,6 +1,6 @@
 function openWaze() {
     const destinationOnPc = "https://www.waze.com/pt-BR/live-map/directions/br/sp/r.-ana-maria-sirani,-546?navigate=yes&to=place.ChIJUaGehy5kzpQRtHIyv_glIIg";
-    const destinationOnMobile = "waze.com/ul/h6gyfp3j4k";
+    const destinationOnMobile = "https://waze.com/ul/h6gyfp3j4k";
     
     const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
