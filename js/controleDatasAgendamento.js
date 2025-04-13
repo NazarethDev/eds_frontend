8fcf524930eb044d86134be36897d0c4727439e1
NazarethDev <lorran.nazareth@gmail.com>
2025-04-13 00:31:44 -0300

    elaboração de formulários em progresso.

---
+++ js/controleDatasAgendamento.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#data').datepicker({
+      format: 'yyyy-mm-dd',
+      daysOfWeekDisabled: [0],
+      autoclose: true,
+      todayHighlight: true
+    });
+  });
