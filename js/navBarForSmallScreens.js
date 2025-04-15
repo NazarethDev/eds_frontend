afe2decc9b2af23ed2d39e1113b88e9976e044ee
NazarethDev <lorran.nazareth@gmail.com>
2025-04-14 22:09:02 -0300

    Responsividade da barra de navegação para telas menores melhorada.

---
+++ js/navBarForSmallScreens.js
@@ -0,0 +1,11 @@
+const navbarToggler = document.getElementById('navbar-toggler');
+const navbarCollapse = document.getElementById('navbarSupportedContent');
+const body = document.body;
+
+navbarToggler.addEventListener('click', function () {
+    if (navbarCollapse.classList.contains('show')) {
+        body.style.paddingTop = '70px'; // Ajuste para a altura da sua navbar
+    } else {
+        body.style.paddingTop = '0'; // Remove o padding-top quando o menu estiver fechado
+    }
+});
